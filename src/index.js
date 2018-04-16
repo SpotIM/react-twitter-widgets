73d407bd25ee91a27b246bb3bd3f1e62bae477d7
Dor Tzur <dor@spot.im>
2018-04-17 01:18:34 +0300

    bump

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import { canUseDOM } from 'exenv'
-import onWindowsLoad from './on-window-load'
+import onWindowLoad from './on-window-load'
 if (canUseDOM) {
   onWindowLoad(()=>{
-  const $script = require('scriptjs') // eslint-disable-line global-require
-  $script('https://platform.twitter.com/widgets.js', 'twitter-widgets')  
+  const $script = require('scriptjs'); // eslint-disable-line global-require
+  $script('https://platform.twitter.com/widgets.js', 'twitter-widgets')
   })
-  
+
 }
 
 export Follow from './components/Follow'
